7b4421b47abec52e9b514bdb349764cb6cfe251b
mohanori <mohamed.nori@ulb.be>
2024-01-15 17:21:37 +0100

    check if gameratg contains special character

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -6,4 +6,24 @@ describe('validations tests suites - isValid', () => {
         const result = isValid(undefined);
         expect(result).toBe(false);
     });
+    test('should return true if gamertag length is equal to 8', () => {
+        const result = isValid("azertyui");
+        expect(result).toBe(true);
+    });
+    test('should return true if gamertag length is more than 8', () => {
+        const result = isValid("azertyui");
+        expect(result).toBe(true);
+    });
+    test('should return false if gamertag length is less than 8', () => {
+        const result = isValid("azer");
+        expect(result).toBe(false);
+    });
+    test('should return false if no special character', () => {
+        const result = isValid("azer");
+        expect(result).toBe(false);
+    });
+    test('should return true if 1 special character', () => {
+        const result = isValid("azer#");
+        expect(result).toBe(true);
+    });
 });
--- src/services/validations.js
+++ src/services/validations.js
@@ -1,9 +1,14 @@
 // TODO: Refactor validation function to follow the defined validations rules
 const isValid = (gamerTag) => {
     if (gamerTag === undefined) return false;
-    if (gamerTag.length < 8) {
+    if (gamerTag.length >= 8) {
         return true;
     }
+
+    if (/[!@#$%^&*(),.?":{}|<>]/.test(gamerTag)) {
+        return true;
+    }
+
     return false;
 };
 
